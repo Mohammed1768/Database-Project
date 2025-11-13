53af7bd18c035f76f976521ae901bc6c642e6304
Swiftyyy404 <mohelewainy@gmail.com>
2025-11-13 20:34:31 +0200

    2.2 DONE and RECHECKED
    Updated SQL scripts to use consistent formatting, standardized view creation statements, and corrected the database name to 'University_HR_ManagementSystem_Team_12'. Improved clarity by adding section comments and fixing the 'allPerformance' view to include the correct WHERE clause.

--- 2.2.sql
+++ 2.2.sql
@@ -1,62 +1,43 @@
+Use University_HR_ManagementSystem_Team_12
+Go
+
+-- 2.2 a)
+Create View allEmployeeProfiles As 
+    Select 
+        employee_ID , first_name , last_name, gender , email, address, years_of_experience, 
+            official_day_off, type_of_contract, employment_status, annual_balance, accidental_balance
+    From Employee;
+Go 
+
+--2.2 b)
+Create View NoEmployeeDept As 
+    Select dept_name, count(employee_ID) as NoOfEmployees
+    from Employee
+    where dept_name is not null
+    group by dept_name;
+Go
+
+--2.2 c)
+Create View allPerformance As 
+    Select * 
+    From Performance
+    Where semester LIKE 'W%';
+Go
+
 --2.2 d)
-USE MS2
-GO
-
-go
--- 2.2 
-Create view allEmployeeProfiles as 
-select 
-employee_ID , first_name , last_name, gender , email, address, years_of_experience, 
-official_day_off, type_of_contract, employment_status, annual_balance, accidental_balance
-
-from Employee;
-go 
-
-create view NoEmployeeDept as 
-select dept_name, count(employee_ID) as NoOfEmployees
-from Employee
-where dept_name is not null
-group by dept_name;
-go
-
-create view allPerformance as 
-select * from Performance;
-where semester LIKE 'W%';
-go
-
-
-
-
-
-
-CREATE VIEW allRejectedMedicals AS
-SELECT
-    ml.request_ID,
-    ml.Emp_ID AS employee_ID,
-    ml.insurance_status,
-    ml.disability_details,
-    ml.type,
-    l.date_of_request,
-    l.start_date,
-    l.end_date,
-    l.num_days,
-    l.final_approval_status
-FROM Medical_Leave ml
-INNER JOIN Leave l ON ml.request_ID = l.request_ID
-WHERE l.final_approval_status = 'rejected';
-GO
+Create View allRejectedMedicals As
+    Select
+        ml.request_ID, ml.Emp_ID AS employee_ID, ml.insurance_status, ml.disability_details, ml.type,
+            l.date_of_request, l.start_date, l.end_date, l.num_days, l.final_approval_status
+    From Medical_Leave ml INNER JOIN Leave l 
+    On ml.request_ID = l.request_ID
+    Where l.final_approval_status = 'rejected';
+Go
 
 -- 2.2 e) 
-CREATE VIEW allEmployeeAttendance AS
-SELECT 
-    attendance_ID,
-    emp_ID,
-    date,
-    check_in_time,
-    check_out_time,
-    total_duration,
-    status
-FROM Attendance
-WHERE date = CAST(DATEADD(day, -1, GETDATE()) AS DATE);
-GO
-
+Create View allEmployeeAttendance As
+    Select 
+        attendance_ID, emp_ID, date, check_in_time, check_out_time, total_duration, status
+    From Attendance
+    Where date = CAST(DATEADD(day, -1, GETDATE()) AS DATE);
+Go
