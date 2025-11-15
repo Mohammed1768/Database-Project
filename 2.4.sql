ddfa5616e126fcd580f02f24dcf7a3713d1a1187
Mohammed1768 <mohammedwissam728@gmail.com>
2025-11-15 22:39:08 +0200

    ...

--- 2.4.sql
+++ 2.4.sql
@@ -148,7 +148,7 @@ begin
 	declare @count int, @rate decimal(10,2), @first_date date
 
 	set @rate = (select top 1 salary from Employee 				-- daily rate = salary / 22 days
-		where @employee_ID = @employee_ID) / (22 * 8);	
+		where @employee_ID = @employee_ID) / 22;	
 
 	set @count = (
 		select count(a.attendance_ID) from Attendance a
@@ -168,47 +168,86 @@ create proc Deduction_unpaid
 @employee_ID int
 as 
 begin
-	create table very_cool_tmp_table_67(start_date date, end_date date, duration int);
-	insert into very_cool_tmp_table_67()
+	declare @CurrentMonthStart date = datefromparts(year(getdate()), month(getdate()), 1);
+    declare @CurrentMonthEnd   date = eomonth(getdate());
 
+
+	create table very_cool_tmp_table_67(start_date date, end_date date, duration int);
+
+	-- all the leaves that overlap with the current month
+	insert into very_cool_tmp_table_67 (start_date, end_date)
+	select l.start_date, l.end_date from 
+	Unpaid_Leave u inner join Leave l on (u.request_ID = l.request_ID) WHERE 
+		l.start_date <= @CurrentMonthEnd and l.end_date >= @CurrentMonthStart
+		and @employee_ID = u.Emp_ID;
+
+	update very_cool_tmp_table_67
+		set start_date = @CurrentMonthStart 
+		where start_date < @CurrentMonthStart;
+
+	update very_cool_tmp_table_67
+		set end_date = @CurrentMonthEnd 
+		where end_date > @CurrentMonthEnd;
+
+	update very_cool_tmp_table_67
+		set duration = end_date - start_date;
+		
+	
+	declare @daily_rate int =  (select top 1 salary from Employee 				-- daily rate = salary / 22 days
+								where @employee_ID = @employee_ID) / 22 ;	
+
+	declare @count int = (select sum(duration) from very_cool_tmp_table_67);
+
+	insert into Deduction(emp_ID, date, amount, type, status, unpaid_ID) 
+		values(@employee_ID, getdate(), @daily_rate * @count, 'unpaid', 'finalized');
+
+	
 end
 go;
 
 create function Bonus_amount(@employee_id int)
 returns int as 
 begin
-	declare @sum int, @rate int, @bonus int;
+	 declare @bonus int;
 
-	set @sum = (
+	declare @count int = (
 		select sum(total_duration) from Attendance a
 		where
 		month(a.date) = month(getdate()) and year(a.date) = year(getdate())
 	);
-	set @rate = (
-		select top 1 salary from Employee where @employee_ID = @employee_ID
-	);
-	
-	set @bonus = (@sum - 22*8) * @rate;
+
+	declare @rate int = (select top 1 salary from Employee e			-- hourly rate
+						  where @employee_id = e.employee_ID) / (22 * 8);
+
+	declare @factor int = 
+			(select max(r.percentage_overtime) from Role r, Employee e, Employee_Role er where	
+			@employee_id = e.employee_ID and @employee_id=er.emp_ID and r.role_name=er.role_name);
+
+	set @bonus = (@count - 22*8) * @rate * @factor * 0.01;
 	if (@bonus <= 0) return 0;
 	return @bonus;
 
 end
 go;
 
+
+-- we are going to assume that the from and to date are the first and last date of the current month 
+-- this is assumed so that the function call to the bonus method is correct
+-- may be changed later
+
 create proc Add_Payroll
 @employee_id int,
-@from date, @to date
-as 
+@from date, @to date	
+as						
 begin
-declare @bonus int, @deduction_amount int, @rate int
 
-set @bonus = dbo.Bonus_amount(@employee_id)	
-set @deduction_amount = (select sum(amount) from Deduction d where d.date<=@to and d.date>=@from)
-set @rate = (select top 1 salary from Employee where @employee_ID = @employee_ID);
+declare @bonus int = dbo.Bonus_amount(@employee_id)	
+declare @deduction_amount int = (select sum(amount) from Deduction d where d.date<=@to and d.date>=@from)
+declare @salary int = (select top 1 salary from Employee where @employee_ID = @employee_ID);
 
 -- payment_date, final_salary_amount, from_date, to_date, comments, bonus_amount, deduction_amount, emp_ID
 insert into Payroll(payment_date, final_salary_amount, from_date, to_date, bonus_amount, deductions_amount, emp_ID) 
-			values(getdate(), 22*8*@rate + @bonus - @deduction_amount, @from, @to, @bonus, @deduction_amount, @employee_id);
+			values(getdate(), @salary + @bonus - @deduction_amount, @from, @to, @bonus, @deduction_amount, @employee_id);
 
 end
 
