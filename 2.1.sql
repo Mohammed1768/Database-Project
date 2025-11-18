3b1a04a9cd5042b0008a9f1935d6f4e7dd0a9a4d
Swiftyyy404 <mohelewainy@gmail.com>
2025-11-18 16:04:06 +0200

    fixed helper method in 2.1

--- 2.1.sql
+++ 2.1.sql
@@ -19,14 +19,29 @@ returns decimal(10,2)
 as 
 begin
 
-declare @base_salary decimal(10,2) = (select top 1 r.base_salary from Employee e inner join Employee_Role er
-		on (e.employee_ID=er.emp_ID) inner join Role r on (r.role_name=er.role_name)
-		where e.employee_ID=@employee_id
-		order by r.rank asc)
+declare @base_salary decimal(10,2) = (
+				select top 1 r.base_salary 
+				from Employee e inner join Employee_Role er on (e.employee_ID=er.emp_ID) 
+				inner join Role r on (r.role_name=er.role_name)
+				where e.employee_ID=@employee_id
+				order by r.rank asc
+				);
 
-declare @YOE int = (select top 1 years_of_experience from Employee where @employee_id=employee_ID);
+declare @YOE int = (
+				select top 1 years_of_experience 
+				from Employee 
+				where @employee_id=employee_ID
+				);
 
-return @base_salary * (1 + (@YOE/100))
+declare @YOE_perc decimal(4,2) = (
+				select top 1 r.percentage_YOE 
+				from Employee e inner join Employee_Role er on (e.employee_ID=er.emp_ID) 
+				inner join Role r on (r.role_name=er.role_name)
+				where e.employee_ID=@employee_id
+				order by r.rank asc
+				);
+
+return @base_salary + (@YOE_perc/100) * @YOE * @base_salary;
 
 end
 go
