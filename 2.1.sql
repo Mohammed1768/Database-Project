beca6acbd5750c38ec5ec95c48850fea9b78cd6d
Swiftyyy404 <mohelewainy@gmail.com>
2025-11-25 12:45:56 +0200

    daddy's HOME

--- 2.1.sql
+++ 2.1.sql
@@ -10,6 +10,39 @@ create database University_HR_ManagementSystem_Team_No_12;
 go
 use University_HR_ManagementSystem_Team_No_12;
 go
+
+-- 2.1 b) helper function: CREATE IT BEFORE CREATING TABLES!
+create or alter function getsalary(@employee_id int)
+returns decimal(10,2)
+as 
+begin
+
+declare @base_salary decimal(10,2) = (
+				select top 1 r.base_salary 
+				from Employee e inner join Employee_Role er on (e.employee_ID=er.emp_ID) 
+				inner join Role r on (r.role_name=er.role_name)
+				where e.employee_ID=@employee_id
+				order by r.rank asc
+				);
+
+declare @YOE int = (
+				select top 1 years_of_experience 
+				from Employee 
+				where @employee_id=employee_ID
+				);
+
+declare @YOE_perc decimal(4,2) = (
+				select top 1 r.percentage_YOE 
+				from Employee e inner join Employee_Role er on (e.employee_ID=er.emp_ID) 
+				inner join Role r on (r.role_name=er.role_name)
+				where e.employee_ID=@employee_id
+				order by r.rank asc
+				);
+
+return @base_salary + (@YOE_perc/100) * @YOE * @base_salary;
+
+end
+go
 -- 2.1 b):
 create or alter proc createAllTables as	
 begin
@@ -253,6 +286,7 @@ begin
 	drop table Employee_Phone;
 	drop table Employee;
 	drop table Department;
+	drop function getsalary;
 end;
 go
 
@@ -261,7 +295,7 @@ go
 create or alter proc dropAllProceduresFunctionsViews as		
 begin
 	-- all functions
-	drop function getsalary, HRLoginValidation, Bonus_amount, EmployeeLoginValidation, MyPerformance,
+	drop function HRLoginValidation, Bonus_amount, EmployeeLoginValidation, MyPerformance,
 	MyAttendance, Last_month_payroll, Deductions_Attendance, Is_On_Leave, Status_leaves
 
 	-- 2.1
@@ -309,41 +343,5 @@ begin
 	truncate table Employee;
 	truncate table Department;
 end;
-go
-
-
-
-
--- helper function:
-create or alter function getsalary(@employee_id int)
-returns decimal(10,2)
-as 
-begin
-
-declare @base_salary decimal(10,2) = (
-				select top 1 r.base_salary 
-				from Employee e inner join Employee_Role er on (e.employee_ID=er.emp_ID) 
-				inner join Role r on (r.role_name=er.role_name)
-				where e.employee_ID=@employee_id
-				order by r.rank asc
-				);
-
-declare @YOE int = (
-				select top 1 years_of_experience 
-				from Employee 
-				where @employee_id=employee_ID
-				);
-
-declare @YOE_perc decimal(4,2) = (
-				select top 1 r.percentage_YOE 
-				from Employee e inner join Employee_Role er on (e.employee_ID=er.emp_ID) 
-				inner join Role r on (r.role_name=er.role_name)
-				where e.employee_ID=@employee_id
-				order by r.rank asc
-				);
-
-return @base_salary + (@YOE_perc/100) * @YOE * @base_salary;
-
-end
 go
 
