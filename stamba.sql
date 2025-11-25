ff89732787f666faa8d5b1c41099bdf5c5ab4bd9
Mohammed1768 <mohammedwissam728@gmail.com>
2025-11-25 18:56:41 +0200

    ...

--- stamba.sql
+++ stamba.sql
@@ -22,17 +22,17 @@ EXEC Submit_annual
 -- check the leave tables and Employee_Approve_Leave table
 SELECT * FROM Annual_Leave a 
 JOIN Leave l ON a.request_ID = l.request_ID 
-where l.request_ID > 50
+where l.request_ID > 84
 
 SELECT * FROM Employee_Approve_Leave el
 JOIN Employee_Role er ON (er.emp_ID = el.Emp1_ID)
-WHERE Leave_ID > 50
+WHERE Leave_ID > 84
 
 
 -- check that the status of the leave after the review from the HR and the Upper board 
 
-exec Upperboard_approve_annual 51, 16, 12;
-exec HR_approval_an_acc 51,5;
+exec Upperboard_approve_annual 85, 16, 12;
+exec HR_approval_an_acc 85,5;
 
 
 -- check the leave tables and Employee_Approve_Leave table
